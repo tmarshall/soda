8cf8640c00f56b790ee47272660899896cdc7da3
tmarshall <timothyjmarshall@gmail.com>
2022-08-27 14:56:48 -0500

    setting up commander usage

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { createServer } from 'node:http'
+import { program } from 'commander'
 
-const server = createServer()
+program
+  .command('walk [directory]')
+  .description('walk a directory of routes')
+  .action((directory = './routes') => {
+    console.log('WALKING', directory)
+  })
 
-server.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({
-    works: true,
-  }))
-})
+program.parse()
 
-server.listen(4000)
-console.log('server listening on 4000')
+// const server = createServer()
+
+// server.on('request', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' })
+//   res.end(JSON.stringify({
+//     works: true,
+//   }))
+// })
+
+// server.listen(4000)
+// console.log('server listening on 4000')
