854e93b807d5c9bc922877f442a7aa2780946c19
tmarshall <timothyjmarshall@gmail.com>
2022-08-28 21:31:23 -0500

    walking dir, need to import files

--- src/walkRoutes.ts
+++ src/walkRoutes.ts
@@ -1,19 +1,31 @@
 import { Dirent } from 'node:fs'
 import { readdir } from 'node:fs/promises'
-import { ReadableStreamGenericReader } from 'node:stream/web'
-import { resolve } from 'node:path'
+import { relative } from 'node:path'
+
+enum RouteVerb {
+  All = 'all',
+  Head = 'head',
+  Get = 'get',
+  Post = 'post',
+  Put = 'put',
+  Patch = 'patch',
+  Delete = 'delete',
+  Options = 'options',
+  Trace = 'trace',
+  Connect = 'connect',
+}
 
 export interface RouteDefinition {
-
+  verb: RouteVerb
 }
 
 export default async function walkRoutes(dirpath = './routes') {
   const baseDir = await readdir(dirpath, { withFileTypes: true })
-  return await walkDirectory(baseDir, baseDir)
+  return await walkDirectory(baseDir, baseDir, dirpath)
 }
 
-async function walkDirectory(baseDir: Dirent[], currentDir: Dirent[]) {
-  const subdirReads: ReadableStreamGenericReader[] = []
+async function walkDirectory(baseDir: Dirent[], currentDir: Dirent[], currentDirPath: string): Promise<RouteDefinition[]> {
+  const subdirsPending: [string, Promise<Dirent[]>][] = []
   const handlers: RouteDefinition[] = []
 
   for (let i = 0; i < currentDir.length; i++) {
@@ -21,11 +33,43 @@ async function walkDirectory(baseDir: Dirent[], currentDir: Dirent[]) {
     
     if (dirent.isDirectory()) {
       if (dirent.name.slice(0, 1) !== '.') {
-        console.log(dirent.name)
+        const subdirPath = relative(currentDirPath, dirent.name)
+        subdirsPending.push([subdirPath, readdir(subdirPath, { withFileTypes: true })])
       }
       continue
     }
+
+    if (!dirent.isFile()) {
+      continue
+    }
+    const filename = dirent.name.slice(-3).toLowerCase()
+    if (filename.slice(-3) !== '.js' && filename.slice(-3) !== '.ts') {
+      continue
+    }
+
+    handlers.push(...getRoutesFromFile(relative(currentDirPath, dirent.name)))
   }
+
+  const pendingSubWalks: Promise<RouteDefinition[]>[] = []
+  for (let i = 0; i < subdirsPending.length; i++) {
+    const [subdirPath, subdirRead] = subdirsPending[i]
+    const subdir = await subdirRead
+    pendingSubWalks.push(walkDirectory(baseDir, subdir, subdirPath))
+  }
+  const subwalks = await Promise.all(pendingSubWalks)
+  
+  return subwalks.reduce((flattened: RouteDefinition[], subdirHandlers: RouteDefinition[]) => {
+    flattened.push(...subdirHandlers)
+    return flattened
+  }, handlers)
+}
+
+function getRoutesFromFile(filePath: string) {
+  const handlers: RouteDefinition[] = []
+
+  // todo: read file content
+
+  return handlers
 }
 
 // // can return undefined
